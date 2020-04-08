51b28ba58987ed47e9c208b73f94967cd97ede23
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-08 00:15:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -20,11 +20,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-  
+muskets = {"muskets" => 2}
+  gun_powder = {"powder" => 4}
   return shipping_manifest
 end
