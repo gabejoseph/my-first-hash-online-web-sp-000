00b8bed5570d68bb19aafad58bd7302b39294466
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-08 00:42:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,6 +15,6 @@ end
 def adding
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   the_manifest["muskets"] = 2 
-  the_manifest["powder"] = 4
+  the_manifest["gun powder"] = 4
   the_manifest
 end
