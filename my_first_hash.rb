0d957c1012a80df81539c4f7355466c4fccdc136
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-08 00:33:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,7 +10,8 @@ def retrieval
   shipping_manifest["oil paintings"]
 end
 
-def adding(shipping_manifest)
+def adding
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
   shipping manifest["muskets"] = 2 
   shipping_manifest["powder"] = 4
 end
