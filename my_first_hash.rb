d3f9a0e2ffcfc82e77ee7148ad058cd2b08524e6
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-08 00:16:45 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,25 +2,17 @@ def my_hash
   hashish = {"stuff" => "double"}
 end
 
-
 def shipping_manifest
   the_manifest = {"what" => "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-muskets = {"muskets" => 2}
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  muskets = {"muskets" => 2}
   gun_powder = {"powder" => 4}
   return shipping_manifest
 end
