858d34e2484192dcab835b8b37d663b606c95ec3
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-08 00:40:24 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,8 +14,6 @@ end
 
 def adding
   the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-  shipping manifest["muskets"] = 2 
-  shipping_manifest["powder"] = 4
-    return shipping_manifest
-    binding.pry
+  the the_manifest["muskets"] = 2 
+  the the_manifest["powder"] = 4
 end
